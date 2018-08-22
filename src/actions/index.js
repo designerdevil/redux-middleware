040f33fdc0eb5a7882249f24729cbbc2d417ddb0
Gaurav <gmall@sapient.com>
2018-08-22 18:27:45 +0530

    Action segregation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,15 @@
 import { SHOW_PAGE_MESSAGE } from './types';
 
-
-export function showMessage(page) {
+const setMessage = (page) => {
   return (dispatch) => {
-    
       dispatch({
         type: SHOW_PAGE_MESSAGE,
         payload: page,
       });
   };
+}
+
+export function showMessage(page) {
+  //Can be a REST API call to get the data based on page and set it in store
+  return setMessage(page);
 }
